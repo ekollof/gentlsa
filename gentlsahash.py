c26872ca4fa08f88e790f5c556c10aa730aa1279
Emiel Kollof <emiel@prolocation.net>
2018-09-07 14:02:50 +0200

    add code :P

---
+++ gentlsahash.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+"""
+Tool for TLSA/DANE
+
+Usage:
+    gentlsahash.py host <name> <port> [--info] [--cloudflare] [--dryrun]
+    gentlsahash.py cloudflare [--info] [--listzones]
+    gentlsahash.py file <certfile>
+
+
+"""
+
+import sys
+import ssl
+import hashlib
+import M2Crypto
+import CloudFlare
+
+from docopt import docopt
+from pprint import pprint
+
+debug = False
+
+
+def getcerthttps(addr, port):
+    conn = ssl.create_connection((addr, port))
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    sock = ctx.wrap_socket(conn, server_hostname=addr)  # SNI
+    pem_cert = ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
+    response = M2Crypto.X509.load_cert_string(pem_cert)
+
+    return response
+
+
+def getcertpubhash(certobj):
+    pubkey = certobj.get_pubkey().as_der()
+    pubkeyhash = hashlib.sha256(pubkey).hexdigest()
+
+    return pubkeyhash
+
+
+def printcertinfo(certobj):
+    print(">>> Certificate Information:")
+    print(f"Serial : {certobj.get_serial_number():x}")
+    print(f"Issuer : {certobj.get_issuer().as_text()}")
+    print(f"Subject: {certobj.get_subject().as_text()}")
+    try:
+        san = certobj.get_ext('subjectAltName')
+        print("Subject Alternative Name(s): %s" % san.get_value())
+    except LookupError:
+        pass
+
+    sig_start = certobj.get_not_before().get_datetime()
+    sig_end = certobj.get_not_after().get_datetime()
+    print(f"Certificate Inception:  {sig_start} {sig_start.tzname()}")
+    print(f"Certificate Expiration: {sig_end} {sig_end.tzname()}")
+    print(f"SHA256 (pubkey): {getcertpubhash(certobj)}")
+
+
+def getcfzonelist(cf):
+    ret = {}
+    zonedata = cf.zones.get()
+    return zonedata
+
+
+def getcfzoneinfo(cf, zonename):
+    ret = {}
+    zonedata = cf.zones.get(params={'name': zonename})
+
+    return zonedata
+
+
+def main():
+    args = docopt(__doc__)
+
+    addr = args['<name>']
+    port = args['<port>']
+
+    if args["--cloudflare"] or args["cloudflare"]:
+        cf = CloudFlare.CloudFlare(debug=debug)
+        zones = getcfzoneinfo(cf, addr)
+        if not zones[0]['name'] is not addr:
+            print("Not managed by cloudflare. Bailing.")
+            return -1
+        if args["--info"]:
+            print(">>> Cloudflare Information:")
+            print(f"Zone name: {addr}")
+            print(f"Zone ID: {zones[0]['id']}")
+            print(f"Zone owner: {zones[0]['owner']['email']}")
+            print(f"Name servers: {zones[0]['name_servers']}")
+
+    certobj = getcerthttps(addr, port)
+    printcertinfo(certobj)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
